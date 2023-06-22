710838adc556bb60799c6cafd1b0aa1090a68db2
Takamasa Hiratsuka <takamasahiratsuka@obniz.com>
2023-06-22 13:41:57 +0900

    complete openocd connection

--- tool/attach_picoprobe.sh
+++ tool/attach_picoprobe.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/sh
 
-set -- "$(usbipd.exe wsl list 2>/dev/null | awk '{if($0 ~ /Picoprobe/){print $0}}')"
-if [ ${#} -gt 0 ] && [ -z "${0}" ] ;
+set -- "$(usbipd.exe wsl list 2>/dev/null | awk 'match($0, /Picoprobe/){print $0}')"
+if [ ${#} -gt 0 ] ;
 then
-    echo "test"
     for i in "${@}"
     do
         BUSID="$(echo "${i}"| awk '{if($0 ~ /Attached/){print $1}}')"
@@ -17,6 +16,7 @@ then
             then
                 echo "attaching ${BUSID}"
                 usbipd.exe wsl attach -b "${BUSID}"
+                echo "attached ${BUSID}"
             fi
         fi
     done
